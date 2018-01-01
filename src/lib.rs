f05810e59e05e510470106b3fff82c0674501595
JunSuzukiJapan <jun.suzuki.japan@gmail.com>
2018-01-01 11:35:57 +0900

    add LICENSE file

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,14 @@
+// Copyright (C) 2017 <Jun Suzuki jun.suzuki.japan@gmail.com>
+//
+// This software may be modified and distributed under the terms
+// of the MIT license.  See the LICENSE file for details.
+//! # char_stream
+//!
+//! ## Examples
+//! ```rust
+//!
+//! ```
+
 mod internals;
 
 use std::str;
